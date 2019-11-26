1dd40889bd8bb8c95e2532a6f1a49c48cc120274
EmoralesS <esteban.morales@outlook.es>
2019-11-26 16:20:03 -0500

    16 - Graficos

---
+++ graficos/src/app/components/dona/dona.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label } from 'ng2-charts';
+
+@Component({
+  selector: 'app-dona',
+  templateUrl: './dona.component.html',
+  styleUrls: ['./dona.component.css']
+})
+export class DonaComponent {
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100],
+    [50, 150, 120],
+    [250, 130, 70],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public numeros_random() {
+    this.doughnutChartData = [
+      [Math.random() * 100,
+      Math.random() * 100,
+      Math.random() * 100],
+      [Math.random() * 100,
+      Math.random() * 100,
+      Math.random() * 100],
+      [Math.random() * 100,
+      Math.random() * 100,
+      Math.random() * 100],
+    ];
+
+  }
+}
