e4cad5d74f2c62d3b077860f030d213610fe1bfa
EmoralesS <esteban.morales@outlook.es>
2019-12-10 10:55:29 -0500

    12 Mapas
    Falta el key

--- mapas/src/app/app.module.ts
+++ mapas/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { MapaComponent } from './components/mapa/mapa.component';
+import { AgmCoreModule } from '@agm/core';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    AgmCoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
